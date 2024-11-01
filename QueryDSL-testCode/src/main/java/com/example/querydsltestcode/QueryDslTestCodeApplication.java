6ca3a19845736a7f4b6cd08f6d2bb10c14ee9903
RyuJeaMin <kariel7673@gmail.com>
2024-11-01 22:00:22 +0900

    Project :: create

---
+++ QueryDSL-testCode/src/main/java/com/example/querydsltestcode/QueryDslTestCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.querydsltestcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslTestCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryDslTestCodeApplication.class, args);
+    }
+
+}
---
+++ QueryDSL-testCode/src/test/java/com/example/querydsltestcode/QueryDslTestCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.querydsltestcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslTestCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
